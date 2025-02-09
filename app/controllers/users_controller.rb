13396046c9b17a90b738da56f8636fdeb699195a
Valentin DUCRETTET <valentin.ducrettet@partagetabanque.fr>
2025-02-09 19:34:12 +0100

    intégration d'une méthode pour télécharger la fiche vcard sur la landing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,22 +33,88 @@ class UsersController < ApplicationController
   end
 
 
-  def whatsapp_url
-    return nil if whatsapp_number.blank?
+  def whatsapp_field(url)
+    return "" unless url.present?
 
-    "https://wa.me/#{whatsapp_number.gsub(/\D/, '')}"
-   end
+    # Nettoyage pour enlever les espaces et ajouter l'indicatif si nécessaire
+    international_number = url.gsub(/\D/, '').sub(/^0/, '33')
+    "URL;TYPE=WhatsApp:https://wa.me/#{international_number}"
+  end
+
+
+   def download_vcard
+    user = User.find(params[:id])
+
+    vcard = <<~VCARD
+      BEGIN:VCARD
+      VERSION:3.0
+      N:#{user.name};#{user.firstname}
+      FN:#{user.firstname} #{user.name}
+      #{format_company(user)}
+      #{format_title(user)}
+      TEL;TYPE=WORK,VOICE:#{user.phone}
+      #{format_email(user)}
+      ADR:;;#{user.address}
+      #{linkedin_field(user.linkedin_url)}
+      #{facebook_field(user.facebook_url)}
+      #{whatsapp_field(user.whatsapp_url)}
+      #{attach_photo(user)}
+      END:VCARD
+    VCARD
+
+    send_data vcard, filename: "#{user.firstname}_#{user.name}.vcf", type: 'text/vcard'
+  end
+
+
+
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :firstname, :photo, :phone, :linkedin_url, :facebook_url, :whatsapp_url, :fiche_contact_url, :address)
+    params.require(:user).permit(:name, :firstname, :photo, :phone, :linkedin_url,
+                                 :facebook_url, :whatsapp_url, :fiche_contact_url,
+                                 :address, :user_company, :user_email)
   end
+
 
   def create_landing_page(user)
     unique_token = SecureRandom.hex(10) # Génère un token unique
     landing_url = unique_token
 
     LandingPage.create(user: user, landing_url: landing_url)
+  end
+
+  def linkedin_field(url)
+    return "" unless url.present?
+    "URL;TYPE=LinkedIn:#{url}"
+  end
+
+  def facebook_field(url)
+    return "" unless url.present?
+    "URL;TYPE=Facebook:#{url}"
+  end
+
+  def whatsapp_field(url)
+    return "" unless url.present?
+    "URL;TYPE=WhatsApp:#{url}"
+  end
+
+  def attach_photo(user)
+    return "" unless user.photo.attached?
+
+    photo_data = Base64.strict_encode64(user.photo.download)
+    "PHOTO;ENCODING=b;TYPE=JPEG:#{photo_data}"
+  end
+
+  def format_company(user)
+    user.user_company.present? ? "ORG:#{user.user_company}" : ""
+  end
+
+  def format_title(user)
+    user.respond_to?(:job_title) && user.job_title.present? ? "TITLE:#{user.job_title}" : ""
+  end
+
+  def format_email(user)
+    user.user_email.present? ? "EMAIL:#{user.user_email}" : ""
   end
 end
