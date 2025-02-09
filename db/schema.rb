f006aafed41dc5017722847c171ee8fb52d5c4c3
Valentin DUCRETTET <valentin.ducrettet@partagetabanque.fr>
2025-02-09 19:50:08 +0100

    Mise à jour du schema après migration AddCompanyAndEmailToUsers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_02_09_182423) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_09_184236) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -68,6 +68,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_09_182423) do
     t.string "whatsapp_url"
     t.string "fiche_contact_url"
     t.string "address"
+    
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "whatsapp_number"
